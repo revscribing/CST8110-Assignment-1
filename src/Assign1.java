5bede411ff20401ecf819e67290c5b514dfb9ea1
Richard Barney <richardbarney90@gmail.com>
2015-03-18 23:31:08 -0400

    upload all files

--- src/Assign1.java
+++ src/Assign1.java
@@ -1,23 +1,32 @@
 /**
-*    @author Richard Barney
-*    Intro to Computer Programming Assignment 1
-*    September 2012
+ * This class simply asks the user to enter a five digit number and displays each 
+ * number individually on a separate line.
+ *  @author Richard Barney
+ *  @version 1.0.0 September 2012
 */
+//import statements
 import java.util.Scanner;
 public class Assign1 {
+	/**
+	 * main method as required by JVM
+	 * @param  args   standard command line parameters as a string array.
+	 */
 	public static void main(String[] args) {
-		//variable declarations
+		// variable declarations
 		Scanner input = new Scanner(System.in);
 		int num;
-		//statement to enter digit
+		
+		// prompr user for number
 		System.out.print("Enter a five digit number: ");
 		num = input.nextInt();
+		
 		//error message if more than 5 digits or less than 5 digits are entered
 		while (num >= 100000 || num <= 9999 ) {
 			System.out.print("Invalid input. Please enter a five digit number: ");
 			num = input.nextInt();
-	    }
+		}
+		
 	    //statement if 5 digits are entered
 		System.out.print("The number is: " +"\n" +num/10000%10 +"\n" +num/1000%10 +"\n" +num/100%10 +"\n" +num/10%10 +"\n" +num%10);
-	}//end main
-}//end class Assign1
+	} // end method main
+} //end class Assign1
